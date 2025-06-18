ba594f76e493c45fd787abb1078b9df7111ff5f9
Luis Espinel <ing.luisespinel@hotmail.com>
2025-06-18 11:00:39 -0500

    ++ rest

---
+++ rest/src/main/java/com/formadoresit/camel/MySpringBootRouter.java
@@ -0,0 +1,47 @@
+package com.formadoresit.camel;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestBindingMode;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A simple Camel route that triggers from a timer and calls a bean and prints to system out.
+ * <p/>
+ * Use <tt>@Component</tt> to make Camel auto detect this route when starting.
+ */
+@Component
+public class MySpringBootRouter extends RouteBuilder {
+
+    @Override
+    public void configure() {
+        restConfiguration()
+                .port(8080)
+                .component("servlet")
+                .bindingMode(RestBindingMode.json)
+                .contextPath("/api/camel");
+
+        rest("/users")
+                .get()
+                    .produces("application/json")
+                    .to("direct:getUsers")
+                // "GET /api/camel/users/{userId}"
+                .get("/{userId}").to("direct:getUserById");
+
+        from("direct:getUsers")
+                .log("procesando get users")
+                .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(200))
+                .process(exchange -> {
+                    Map<String, Object> body = new HashMap<>();
+                    body.put("message", "ok");
+                    exchange.getMessage().setBody(body);
+                });
+
+        from("direct:getUserById")
+                .log("procesando get user by id");
+    }
+
+}
