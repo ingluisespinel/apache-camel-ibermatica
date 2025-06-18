4a6e4ecf50407f91507c7362e319a7eab5aaeb9b
Luis Espinel <ing.luisespinel@hotmail.com>
2025-06-18 16:59:24 -0500

    ++ open api dependencies

--- rest/src/main/java/com/formadoresit/camel/MySpringBootRouter.java
+++ rest/src/main/java/com/formadoresit/camel/MySpringBootRouter.java
@@ -3,6 +3,7 @@ package com.formadoresit.camel;
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
+import org.apache.camel.model.rest.RestParamType;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
@@ -18,18 +19,30 @@ public class MySpringBootRouter extends RouteBuilder {
 
     @Override
     public void configure() {
+        /*
         restConfiguration()
                 .port(8080)
                 .component("servlet")
                 .bindingMode(RestBindingMode.json)
                 .contextPath("/api/camel");
 
+        restConfiguration()
+                .apiProperty("api.title", "Camel REST")
+                .apiProperty("api.description", "API REST Users By Camel");
+
+         */
+
         rest("/users")
                 .get()
                     .produces("application/json")
                     .to("direct:getUsers")
                 // "GET /api/camel/users/{userId}"
-                .get("/{userId}").to("direct:getUserById");
+                .get("/{userId}")
+                    .param()
+                        .name("userId")
+                        .type(RestParamType.path).description("User Id to find")
+                    .endParam()
+                    .to("direct:getUserById");
 
         from("direct:getUsers")
                 .log("procesando get users")
